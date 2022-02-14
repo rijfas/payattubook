948c210272d5b4cab06e41c5257a98123825e7e1
rijfas <rijfas01@gmail.com>
2022-02-14 09:02:45 +0530

    feat:onboard screen rework

--- lib/presentation/screens/onboarding_screen/onboarding_screen.dart
+++ lib/presentation/screens/onboarding_screen/onboarding_screen.dart
@@ -1,29 +1,15 @@
 import 'package:flutter/material.dart';
-import 'package:smooth_page_indicator/smooth_page_indicator.dart';
+import 'package:flutter/services.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:introduction_screen/introduction_screen.dart';
+import 'package:payattubook/presentation/router/app_router.dart';
 
 import '../../../core/constants/assets.dart';
-import '../../router/app_router.dart';
-import 'components/slide.dart';
+import '../../../core/themes/app_theme.dart';
 
 class OnboardingScreen extends StatefulWidget {
   const OnboardingScreen({Key? key}) : super(key: key);
-  final List<Widget> slides = const [
-    Slide(
-      slide: Assets.firstOnboardImage,
-      title: 'sdfsdf sdfsdf sdfdf',
-      subTitle: 'sdfsdfsdfs sdfsdfd dsfsdf sdfsdfsd fsdfsdfsdf',
-    ),
-    Slide(
-      slide: Assets.secondOnboardImage,
-      title: 'sdfsffgffh gfhfghg gfh',
-      subTitle: 'sdfsdfsdfs sdfhsdfj sfkjsdlkfjs kcjvjc',
-    ),
-    Slide(
-      slide: Assets.thirdOnboardImage,
-      title: 'Test sdfsdf sdfsdf',
-      subTitle: 'sdfsdfsdfs sfsdfj sdfsfd sdffsdf',
-    ),
-  ];
+
   @override
   State<OnboardingScreen> createState() => _OnboardingScreenState();
 }
@@ -32,95 +18,92 @@ class _OnboardingScreenState extends State<OnboardingScreen> {
   @override
   void initState() {
     super.initState();
-    _controller = PageController();
+    SystemChrome.setSystemUIOverlayStyle(
+      SystemUiOverlayStyle.dark.copyWith(statusBarColor: Colors.transparent),
+    );
   }
 
   @override
   void dispose() {
-    _controller.dispose();
+    SystemChrome.setSystemUIOverlayStyle(
+      SystemUiOverlayStyle.dark,
+    );
     super.dispose();
   }
 
-  late final PageController _controller;
-
   @override
   Widget build(BuildContext context) {
+    const bodyStyle = TextStyle(fontSize: 19.0);
+    const pageDecoration = PageDecoration(
+      titleTextStyle: TextStyle(fontSize: 28.0, fontWeight: FontWeight.w700),
+      bodyTextStyle: bodyStyle,
+      bodyPadding: EdgeInsets.fromLTRB(16.0, 0.0, 16.0, 16.0),
+      pageColor: Colors.white,
+      imagePadding: EdgeInsets.zero,
+    );
+    final Size size = MediaQuery.of(context).size;
     return Scaffold(
       body: SafeArea(
-        child: Padding(
-          padding: const EdgeInsets.all(20.0),
-          child: Column(
-            mainAxisSize: MainAxisSize.max,
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  const Text(
-                    'Payattu Book',
-                    style:
-                        TextStyle(fontWeight: FontWeight.bold, fontSize: 32.0),
-                  ),
-                  InkWell(
-                    onTap: () => Navigator.of(context).pushNamedAndRemoveUntil(
-                        AppRouter.signInScreen, (route) => false),
-                    child: const Text(
-                      'Skip',
-                      style: TextStyle(fontWeight: FontWeight.bold),
-                    ),
-                  ),
-                ],
+        child: IntroductionScreen(
+          showDoneButton: true,
+          pages: [
+            PageViewModel(
+              title: "Fractional shares",
+              body:
+                  "Instead of having to buy an entire share, invest any amount you want.",
+              image: SvgPicture.asset(
+                Assets.firstOnboardImage,
+                width: size.width * 0.4,
               ),
-              Flexible(
-                fit: FlexFit.tight,
-                child: PageView(
-                  controller: _controller,
-                  children: widget.slides,
-                ),
+              decoration: pageDecoration,
+            ),
+            PageViewModel(
+              title: "Learn as you go",
+              body:
+                  "Download the Stockpile app and master the market with our mini-lesson.",
+              image: SvgPicture.asset(
+                Assets.secondOnboardImage,
+                width: size.width * 0.4,
               ),
-              SmoothPageIndicator(
-                controller: _controller,
-                count: widget.slides.length,
-                effect: WormEffect(
-                  offset: 8.0,
-                  dotWidth: 8.0,
-                  dotHeight: 8.0,
-                  spacing: 4.0,
-                  radius: 8,
-                  dotColor: Theme.of(context).disabledColor,
-                  activeDotColor: Theme.of(context).primaryColor,
-                ),
+              decoration: pageDecoration,
+            ),
+            PageViewModel(
+              title: "Kids and teens",
+              body:
+                  "Kids and teens can track their stocks 24/7 and place trades that you approve.",
+              image: SvgPicture.asset(
+                Assets.thirdOnboardImage,
+                width: size.width * 0.4,
               ),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.end,
-                children: [
-                  SizedBox(
-                    height: 64,
-                    width: 64,
-                    child: ElevatedButton(
-                      style:
-                          ElevatedButton.styleFrom(shape: const CircleBorder()),
-                      onPressed: () {
-                        if (_controller.page!.toInt() ==
-                            widget.slides.length - 1) {
-                          Navigator.of(context).pushNamedAndRemoveUntil(
-                              AppRouter.signInScreen, (route) => false);
-                        } else {
-                          _controller.nextPage(
-                              duration: const Duration(milliseconds: 200),
-                              curve: Curves.easeInOut);
-                        }
-                      },
-                      child: const Icon(
-                        Icons.arrow_right_alt_outlined,
-                        color: Colors.white,
-                        size: 36,
-                      ),
-                    ),
-                  )
-                ],
-              )
-            ],
+              decoration: pageDecoration,
+            ),
+          ],
+          onDone: () => Navigator.of(context).pushNamedAndRemoveUntil(
+              AppRouter.signInScreen, (route) => false),
+          onSkip: () => Navigator.of(context).pushNamedAndRemoveUntil(
+              AppRouter.signInScreen, (route) => false),
+          showSkipButton: true,
+
+          skipOrBackFlex: 0,
+          nextFlex: 0,
+          showBackButton: false,
+          //rtl: true, // Display as right-to-left
+          back: const Icon(Icons.arrow_back),
+          skip:
+              const Text('Skip', style: TextStyle(fontWeight: FontWeight.w600)),
+          next: const Icon(Icons.arrow_forward),
+          done:
+              const Text('Done', style: TextStyle(fontWeight: FontWeight.w600)),
+          curve: Curves.fastLinearToSlowEaseIn,
+          controlsMargin: const EdgeInsets.all(16),
+          controlsPadding: const EdgeInsets.all(12.0),
+          dotsDecorator: const DotsDecorator(
+            size: Size(10.0, 10.0),
+            color: AppTheme.lightPrimaryColor,
+            activeSize: Size(22.0, 10.0),
+            activeShape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.all(Radius.circular(25.0)),
+            ),
           ),
         ),
       ),
